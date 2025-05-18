8c196d30b5003e93129d6b4b3b77da9593efbc12
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-18 17:40:27 +0900

    [Feat] #9 - 회원가입 API 구현

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('health', views.health_check, name='health_check'),
+    path('auth/register', views.register, name='register'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password, check_password
 
+from .models import User, File, Chat, Message
+
+import json
+
 # Create your views here.
 
 @csrf_exempt
@@ -12,3 +16,70 @@ def health_check(request):
         "message": "server connection sucess",
         "data": None
     })
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        user_id = body.get('user_id')
+        user_email = body.get('user_email')
+        user_password = body.get('user_password')
+        user_name = body.get('user_name')
+        user_category = body.get('user_category')
+        
+        if user_category is None:
+            user_category = 0
+        
+        # 값이 비어있다면 400 오류
+        if not user_id or not user_email or not user_password or not user_name \
+            or user_id == "" or user_email == "" or user_password == "" or user_name == "":
+            return JsonResponse({
+                "response": 400,
+                "message": "missing required fields",
+                "data": None
+            })
+            
+        # 이미 존재하는 아이디, 이메일인지 확인
+        if User.objects.filter(user_id=user_id).exists():
+            return JsonResponse({
+                "response": 409,
+                "message": "user id already exists",
+                "data": None
+            })
+        
+        if User.objects.filter(user_email=user_email).exists():
+            return JsonResponse({
+                "response": 409,
+                "message": "email already exists",
+                "data": None
+            })
+            
+        # 비밀번호 암호화
+        hashed_password = make_password(user_password)
+        
+        # User 객체 생성
+        user = User(
+            user_id=user_id,
+            user_email=user_email,
+            user_password=hashed_password,
+            user_name=user_name,
+            user_category=user_category
+        )
+        
+        # User 객체 저장
+        user.save()
+        
+        # 회원가입 성공
+        return JsonResponse({
+            "response": 200,
+            "message": "register success",
+            "data": None
+        })
+        
+    else:
+        # 잘못된 요청
+        return JsonResponse({
+            "response": 405,
+            "message": "method not allowed",
+            "data": None
+        })
