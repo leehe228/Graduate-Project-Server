a36637ad1612a32f620f9ee8c86f6fd21fa66471
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-18 17:40:16 +0900

    [Feat] #9 - Server Health Check API 구현

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('health', views.health_check, name='health_check'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
+
+@csrf_exempt
+def health_check(request):
+    return JsonResponse({
+        "response": 200,
+        "message": "server connection sucess",
+        "data": None
+    })
