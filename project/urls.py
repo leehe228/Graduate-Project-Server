3ebb5411ceaf12c12e58a7947bb45b1b5e66aa4a
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-18 18:36:24 +0900

    [Setting] #11 - static url 세팅

--- project/urls.py
+++ project/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('api/', include('api.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
