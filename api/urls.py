2379524ab1b5302b97abe3612c925293d903559e
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-19 05:26:22 +0900

    [Feat] #15 - 채팅 API 구현

--- api/urls.py
+++ api/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('files/upload', views.upload_file, name='upload_file'),
     path('files/list', views.list_files, name='list_files'),
     path('files/delete', views.delete_file, name='delete_file'),
+    path('chat/start', views.start_chat, name='start_chat'),
+    path('chat/query', views.query_chat, name='query_chat'),
+    path('chat/list', views.list_chats, name='list_chats'),
+    path('chat/history', views.get_chat_history, name='get_chat_history'),
+    path('chat/delete', views.delete_chat, name='delete_chat'),
 ]
